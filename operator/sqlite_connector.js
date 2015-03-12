553d2cf174fd210bbd2a09529451e1f4dcc5e33d
nhnghia <nhnghia@192.168.0.18>
2015-03-12 17:51:46 +0100

    Runnable with traffic stats (Dashboard, Hierachy, Categories, Applications). Not yet with Flow (Flow cloud, Flow avg, Flow density)

--- operator/sqlite_connector.js
+++ operator/sqlite_connector.js
@@ -1,6 +1,7 @@
 var sqlite3 = require('sqlite3').verbose();
 var moment = require('moment');
-var db = new sqlite3.Database('./mmt.db');
+var db = new sqlite3.Database('./mmt.sqlite');
+var dataAdaptor = require("./dataAdaptor.js");
 
 /**  TODO:
  * - Table for flow stats
@@ -8,12 +9,15 @@ var db = new sqlite3.Database('./mmt.db');
  * - Table for SSL stats
  * - Table for RTP stats
  **/
-  
-var createTrafficStatsTable = "CREATE TABLE IF NOT EXISTS traffic (format INTEGER, ts INTEGER, proto INTEGER, path TEXT, scount INTEGER, ascount INTEGER, data INTEGER, payload INTEGER, packets INTEGER)";
-var createTrafficMinStatsTable = "CREATE TABLE IF NOT EXISTS traffic_min (format INTEGER, ts INTEGER, proto INTEGER, path TEXT, scount INTEGER, ascount INTEGER, data INTEGER, payload INTEGER, packets INTEGER)";
-var createTrafficHourStatsTable = "CREATE TABLE IF NOT EXISTS traffic_hour (format INTEGER, ts INTEGER, proto INTEGER, path TEXT, scount INTEGER, ascount INTEGER, data INTEGER, payload INTEGER, packets INTEGER)";
-var createUniqueIndexMinStats = "CREATE UNIQUE INDEX IF NOT EXISTS stats_min_index ON traffic_min(format, ts, proto, path)";
-var createUniqueIndexHourStats = "CREATE UNIQUE INDEX IF NOT EXISTS stats_hour_index ON traffic_hour(format, ts, proto, path)";
+
+var columns = "format INTEGER, proto INTEGER, source TEXT, path TEXT, time INTEGER, " +
+		"flowcount INTEGER, active_flowcount INTEGER, app TEXT, " +
+		"bytecount INTEGER, payloadcount INTEGER, packetcount INTEGER";
+var createTrafficStatsTable = "CREATE TABLE IF NOT EXISTS traffic (" + columns + ")"; 
+var createTrafficMinStatsTable = "CREATE TABLE IF NOT EXISTS traffic_min ("  + columns + ")";
+var createTrafficHourStatsTable = "CREATE TABLE IF NOT EXISTS traffic_hour ("  + columns + ")";
+var createUniqueIndexMinStats = "CREATE UNIQUE INDEX IF NOT EXISTS stats_min_index ON traffic_min(format, time, proto, path)";
+var createUniqueIndexHourStats = "CREATE UNIQUE INDEX IF NOT EXISTS stats_hour_index ON traffic_hour(format, time, proto, path)";
 
 function prepareDB ( options ) {
   db.run( createTrafficStatsTable );
@@ -30,25 +34,30 @@ function prepareDB ( options ) {
  * - Update RTP stats history
  **/
 function updateInsertHistory(msg, table, period) {
-  var stmt = db.prepare("INSERT OR REPLACE INTO " + table + " (format, ts, proto, path, scount, ascount, data, payload, packets) "
-    + " VALUES (?1, ?2, ?3, ?4, ?5, ?6 "
-    + ", COALESCE( (SELECT data FROM " + table + " WHERE (format = ?1 AND ts = ?2 AND proto = ?3 AND path = ?4) ) + ?7, ?7) "
-    + ", COALESCE( (SELECT payload FROM " + table + " WHERE (format = ?1 AND ts = ?2 AND proto = ?3 AND path = ?4) ) + ?8, ?8) "
-    + ", COALESCE( (SELECT packets FROM " + table + " WHERE (format = ?1 AND ts = ?2 AND proto = ?3 AND path = ?4) ) + ?9, ?9) ) ");
-  stmt.run(msg.event.format, moment(msg.event.ts).startOf(period).valueOf(), msg.event.proto, msg.event.path, msg.event.scount, msg.event.ascount, msg.event.data, msg.event.payload, msg.event.packets);
+  var stmt = db.prepare("INSERT OR REPLACE INTO " + table 
+    + " VALUES (?1, ?2, ?3, ?4, ?5, ?6, ?7, ?8" //" , ?9, ?10, ?11 "
+    + ", COALESCE( (SELECT bytecount    FROM " + table + " WHERE (format = ?1 AND proto = ?2 AND path = ?4 AND time = ?5) ) + ?9, ?9) "
+    + ", COALESCE( (SELECT payloadcount FROM " + table + " WHERE (format = ?1 AND proto = ?2 AND path = ?4 AND time = ?5) ) + ?10, ?10) "
+    + ", COALESCE( (SELECT packetcount  FROM " + table + " WHERE (format = ?1 AND proto = ?2 AND path = ?4 AND time = ?5) ) + ?11, ?11) ) ");
+  
+  stmt.run( msg.format, msg.probe, msg.source, msg.path, moment(msg.time).startOf('minute').valueOf(),
+		  msg.flowcount, msg.active_flowcount, msg.app, msg.bytecount, msg.payloadcount, msg.packetcount);
   stmt.finalize();
 }
 
 /**  TODO:
- * - Insert flow stats
+ * - Insert flow stas
  * - Insert Web stats
  * - Insert SSL stats
  * - Insert RTP stats
  **/
 
 function addProtocolStats( msg ) {
-  var stmt = db.prepare("INSERT INTO traffic VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)");
-  stmt.run( msg.event.format, msg.event.ts, msg.event.proto, msg.event.path, msg.event.scount, msg.event.ascount, msg.event.data, msg.event.payload, msg.event.packets);
+	if (msg.format != 99)
+		return;
+  var stmt = db.prepare("INSERT INTO traffic VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+  stmt.run( msg.format, msg.probe, msg.source, msg.path, msg.time, 
+		  msg.flowcount, msg.active_flowcount, msg.app, msg.bytecount, msg.payloadcount, msg.packetcount);
   stmt.finalize();
 
   updateInsertHistory( msg, "traffic_min", "minute" );
@@ -62,11 +71,12 @@ function addProtocolStats( msg ) {
  * - Retreive RTP stats
  **/
 function getProtocolStats( options, callback ){
-  db.all("SELECT * from " + options.collection + " WHERE (format == 99 AND ts >= " + options.ts + ") ", function( err, doc ) {
-    if (err) callback ( 'InternalError' );
+	var sqlStr = "SELECT * from " + options.collection + " WHERE (format == 99 AND time >= " + options.time + ") ";
+  db.all(sqlStr, function( err, doc ) {
+    if (err) callback ( 'InternalError' + err + ": " + sqlStr);
     var data = [];
     for(i in doc) {
-      var record = [doc[i].format, doc[i].ts, doc[i].proto, doc[i].path, doc[i].scount, doc[i].ascount, doc[i].data, doc[i].payload, doc[i].packets];
+      var record = dataAdaptor.reverseFormatReportItem( doc[i] );
       data.push(record);
     } 
     callback (null, { metadata: {numberOfEntries: data.length}, data: data } );
