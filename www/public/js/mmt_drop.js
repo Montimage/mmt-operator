9c15f160b147ee1d3a1e741880d8ab31be52015b
Huu Nghia NGUYEN <huunghia.nguyen@montimage.com>
2016-04-19 17:31:35 +0200

    fix order of elems in tooltip

--- www/public/js/mmt_drop.js
+++ www/public/js/mmt_drop.js
@@ -5348,6 +5348,31 @@ MMTDrop.chartFactory = {
 		        			format: {
 		        				title: MMTDrop.tools.formatDateTime
 		        			},
+                            contents: function (d, defaultTitleFormat, defaultValueFormat, color) {
+                                  var $$ = this, config = $$.config,
+                                      titleFormat = config.tooltip_format_title || defaultTitleFormat,
+                                      nameFormat = config.tooltip_format_name || function (name) { return name; },
+                                      valueFormat = config.tooltip_format_value || defaultValueFormat,
+                                      text, i, title, value, name, bgcolor;
+                                  for (i = 0; i < d.length; i++) {
+                                      if (! (d[i] && (d[i].value || d[i].value === 0))) { continue; }
+
+                                      if (! text) {
+                                          title = titleFormat ? titleFormat(d[i].x) : d[i].x;
+                                          text = "<table class='" + $$.CLASS.tooltip + "'>" + (title || title === 0 ? "<tr><th colspan='2'>" + title + "</th></tr>" : "");
+                                      }
+
+                                      name = nameFormat(d[i].name);
+                                      value = valueFormat(d[i].value, d[i].ratio, d[i].id, d[i].index);
+                                      bgcolor = $$.levelColor ? $$.levelColor(d[i].value) : color(d[i].id);
+
+                                      text += "<tr class='" + $$.CLASS.tooltipName + "-" + d[i].id + "'>";
+                                      text += "<td class='name'><span style='background-color:" + bgcolor + "'></span>" + name + "</td>";
+                                      text += "<td class='value'>" + value + "</td>";
+                                      text += "</tr>";
+                                  }
+                                  return text + "</table>";
+                              },
                             grouped: true
 		        		},
 		        		grid: {
