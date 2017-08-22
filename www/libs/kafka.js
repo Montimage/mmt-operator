9ccaa919fdd8de831e72f3e070367238f779f1ca
Huu Nghia NGUYEN <huunghia.nguyen@montimage.com>
2017-08-22 11:45:17 +0200

    separate kafka consumer/producer, fix bugs

--- www/libs/kafka.js
+++ www/libs/kafka.js
@@ -3,7 +3,8 @@
 
 const fs     = require("fs");
 const config = require("./config");
-const kafka = require('kafka-node');
+const tools  = require("./tools");
+const kafka  = require('kafka-node');
 
 const kafkaConnectionString = config.kafka_input.host + ":" + config.kafka_input.port;
 const zkOptions = undefined;// {};
@@ -11,128 +12,159 @@ const noAckBatchOptions =  undefined;// {};
 const sslOptions = {};
 
 if( config.kafka_input["ssl.ca.location"] ){
-	sslOptions.ca = fs.readFileSync( config.kafka_input["ssl.ca.location"] );
-	//donot verify hostname
-	sslOptions.rejectUnauthorized = false;
+   sslOptions.ca = fs.readFileSync( config.kafka_input["ssl.ca.location"] );
+   //donot verify hostname
+   sslOptions.rejectUnauthorized = false;
 }
 
-function createClient(){
-	var client = new kafka.Client( 
-			kafkaConnectionString, 
-			"mmt-operator-" + (new Date()).getTime(),
-			zkOptions,
-			noAckBatchOptions,
-			sslOptions
-	);
-	var ret = {};
-	ret.topics = [];
-	
-	ret.consumer = new kafka.Consumer(
-			client,
-			[],
-			{
-				autoCommit: true,
-				fromOffset: "latest",
-				encoding  : 'utf8'
-			}
-	),
-	ret.producer = new kafka.Producer( client );
+/**
+ * 
+ * @param type either "producer", "consumer" or "both"
+ * @returns
+ */
+function createClient( type, clientName  ){
 
-	ret.producer._ready  = false;
-	ret.consumer._ready  = false;
-	ret.consumer._topics = [];
-	ret.producer._data   = [];
-	
-	ret.producer.on("ready", onReady ); 
-	ret.consumer.on('ready', onReady );
-	
-	
-	function onReady(){
-		//create topics if they do not exist
-		if( ret.topics.length > 0 ){
-			ret.producer.createTopics( ret.topics, function( err, data){
-				if( err )
-					console.error( err );
-			});
-		}
-		
-		//console.log( this == ret.consumer );
-		//console.log( this );
-		
+   const isNeedConsumer = ( type != "producer" );
+   const isNeedProducer = ( type != "consumer" );
 
-		ret.producer._ready = true;
-		ret.consumer._ready = true;
-		
-		console.log("Ready !!!!", ret.topics );
-		
-		//subscribe to topics
-		for( var i in ret.consumer._topics )
-			ret.subscribe( ret.consumer._topics[i] );
-			
-		
-		for( var i in ret.producer._data )
-			ret.publish( ret.producer._data[i] );
-	}
-	
-	function onError( err ){
-		console.error("Kafka Error", err );
-	}
-	
-	ret.consumer.on('error', onError );
-	ret.producer.on('error', onError );
-
-	/**
-	 * subscribe to a new channel
-	 * - channel: name of channel to subscribe
-	 * - cb     : callback( err, added )
-	 */
-	ret.subscribe = function( channel ){
-		console.log( "subscribe", channel, ret.consumer._ready );
-		if( ret.consumer._ready ) {
-			ret.consumer.addTopics([channel], function( err, added ){
-				if( err )
-					console.error( err );
-			});
-		}
-		else{
-			ret.topics.push( channel );
-			ret.consumer._topics.push( channel );
-		}
-	};
+   //name of kafka client
+   if( clientName == undefined )
+      clientName = "mmt-operator-" + tools.getTimestamp();
+   else
+      clientName = "mmt-operator-" + clientName;
+   
+   var client = new kafka.Client( 
+         kafkaConnectionString, 
+         clientName,
+         zkOptions,
+         noAckBatchOptions,
+         sslOptions
+   );
+   
+   const ret = { topics: [], clientName : clientName };
+   
+   
+   function onError( err ){
+      console.error("Kafka Error", err );
+   }
 
 
-//	override "on" function
-	ret.on = function( ev, cb ){
-		if( ev === "message" )
-			ret.consumer.on("message", function( msg ){
-				//console.log( msg );
-				cb( msg.topic, msg.value );
-			})
-			else
-				ret.consumer.on( ev, cb );
-	};
+   //1. consumer
+   if( isNeedConsumer ){
+
+      //create kafka client
+      ret.consumer = new kafka.Consumer(
+            client,
+            [],
+            {
+               //consumer group id, default `kafka-node-group`
+               groupId: 'kafka-node-group', 
+               // Auto commit config 
+               autoCommit: true,
+               autoCommitIntervalMs: 5000,
+               // The max wait time is the maximum amount of time in milliseconds to block waiting if insufficient data is available at the time the request is issued, default 100ms 
+               fetchMaxWaitMs: 100,
+               // This is the minimum number of bytes of messages that must be available to give a response, default 1 byte 
+               fetchMinBytes: 1,
+               // The maximum bytes to include in the message set for this partition. This helps bound the size of the response. 
+               fetchMaxBytes: 1024 * 1024,
+               // If set true, consumer will fetch message from the given offset in the payloads 
+               fromOffset: false,
+               // If set to 'buffer', values will be returned as raw buffer objects. 
+               encoding: 'utf8',
+               keyEncoding: 'utf8'
+            }
+      ),
+      //init kafka client's variables
+      ret.consumer._topics = [];
+
+      console.log("KafkaConsumer ["+ ret.clientName +"] is ready !!!!", ret.consumer._topics );
 
 
-	ret.publish = function( topic, message, cb ){
-		cb = cb || function( err, data ){ 
-			if( err )
-				return console.error( err );
-			
-			//console.log( data );
-		};
-		var data = [{topic: topic, messages: message}];
-		
-		if( ret.producer._ready )
-			ret.producer.send( data, cb );
-		else{
-			ret.topics.push( topic );
-			ret.producer._data.push( data );
-		}
-	};
-	
-	return ret;
+      /**
+       * subscribe to a new channel
+       * - channel: name of channel to subscribe
+       * - cb     : callback( err, added )
+       */
+      ret.subscribe = function( channel ){
+         console.log( "subscribe", channel );
+         ret.consumer.addTopics([channel], function( err, added ){
+            if( err )
+               console.error( err );
+         });
+      };
+
+
+      //override "on" function
+      ret.on = function( ev, cb ){
+         if( ev === "message" )
+            ret.consumer.on("message", function( msg ){
+               //console.log( msg );
+               cb( msg.topic, msg.value );
+            });
+         else
+            ret.consumer.on( ev, cb );
+      };
+      
+      //on error
+      ret.consumer.on('error', onError );
+   }
+
+   //2. producer
+   if( isNeedProducer ){
+      ret.producer = new kafka.Producer( client );
+      
+      ret.producer._ready  = false;
+      ret.producer._topics = [];
+      ret.producer._data   = [];
+
+      ret.producer.on("ready", function(){
+         //create topics if they do not exist
+         if( ret.topics.length > 0 ){
+            ret.producer.createTopics( ret.topics, function( err, data){
+               if( err )
+                  console.error( err );
+            });
+         }
+
+         ret.producer._ready = true;
+
+         console.log("KafkaProducer ["+ ret.clientName +"] is ready !!!!", ret.producer._topics );
+
+         //publish cache's data
+         for( var i in ret.producer._data ){
+            var o = ret.producer._data[i];
+            ret.producer.send( o.data, o.cb );
+         }
+         //empty _cache
+         ret.producer._data = null;
+      }); 
+
+
+      ret.publish = function( topic, message, cb ){
+         cb = cb || function( err, data ){ 
+            if( err )
+               return console.error( err );
+
+            //console.log( data );
+         };
+         const data = [{topic: topic, messages: message}];
+
+         if( ret.producer._ready )
+            ret.producer.send( data, cb );
+         else{
+            ret.topics.push( topic );
+            ret.producer._topics.push( topic );
+            //save to the cache
+            ret.producer._data.push( {data: data, cb: cb} );
+         }
+      };
+
+      ret.producer.on('error', onError );
+   }
+   return ret;
 }
 
 module.exports = {
-		createClient: createClient
+      createClient: createClient
 };
