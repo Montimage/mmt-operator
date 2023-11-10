1e321473642cfc60064eb8b2f31abc874dce5132
nhnghia <nhnghia@localhost>
2023-11-10 15:51:09 +0100

    [Mosaico] empty also security alerts when unblocking attacks

--- www/data/attack/script/l4s-unblock-traffic.sh
+++ www/data/attack/script/l4s-unblock-traffic.sh
@@ -12,6 +12,11 @@ IPs=$2
 # Run any command as you want here, e.g.,
 # echo $IPs | ssh root@tata -- python3 unblock.py
 
+# clear P4 block list
 # the output will be shown back to user via Web GUI
-echo "table_delete tb_blocklist 0" | simple_switch_CLI | tee -a /tmp/l4s-unblock-traffic.log
-echo "Unblocked successfully IPs: $IPs"
+echo "table_clear tb_blocklist" | simple_switch_CLI | tee -a /tmp/l4s-unblock-traffic.log
+echo "Unblocked successfully all IPs"
+
+# empty database
+mongo mmt-data --eval 'db.security.drop()' >> /tmp/l4s-unblock-traffic.log
+echo "Cleaned alerts"
