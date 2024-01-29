16748d3f9e8f7eb2ebdadd008ecf811387597621
Ivan <ivanforzajuve@gmail.com>
2024-01-29 15:39:14 +0100

    Updated tab for rca

--- www/public/js/chart/tab.rca.js
+++ www/public/js/chart/tab.rca.js
@@ -5,7 +5,7 @@ var arr = [
       x: 0,
       y: 6,
       width: 12,
-      height: 7,
+      height: 8,
       type: "info",
       userData: {
          fn: "createRcaReport"
@@ -24,12 +24,15 @@ MMTDrop.callback = {
       }
 };
 
+var dataValues =[];
+var middleIndex = undefined;
+
 
 var ReportFactory = {};
 ReportFactory.createRcaReport = function(fPeriod){
    const COL = MMTDrop.constants.SecurityColumn;
    const database = new MMTDrop.Database({
-      collection: "rca",  //name of collection which must be inside "mmt-data" database
+      collection: "report",  //name of collection which must be inside "mmt-data" database
       action: "find",
       no_group : true, 
       query: [],
@@ -38,6 +41,7 @@ ReportFactory.createRcaReport = function(fPeriod){
    }, function( data ){
       //got data from DB
       console.log(data)
+      dataValues = data
       //do any processing here if need
       return data;
    }, false);
@@ -55,7 +59,7 @@ ReportFactory.createRcaReport = function(fPeriod){
      console.log( fProbe.selectedOption().id )
 
      //  example: select only the documents having  1 < _id < 10
-      const $match = { _id: {$gt: 1, $lt: 10}};
+      const $match = { _id: {$gt: 0}};
 
       return {query: [{$match: $match}]};
    };
@@ -65,21 +69,30 @@ ReportFactory.createRcaReport = function(fPeriod){
       getData: {
          getDataFn: function (db) {
             var data = db.data();
+            // console.log(data[0])
+            if(data[0].id!==0){
+               data.unshift({"_id":0,"KnownIncidentID":"0"}) //in this way the graph starts always in the origin if no point with x=0 is present
+            }
+            // valuesToPush = [{"_id":10,"KnownIncidentID":0},{"_id":11,"KnownIncidentID":0},{"_id":12,"KnownIncidentID":0},{"_id":13,"KnownIncidentID":3},{"_id":14,"KnownIncidentID":3},{"_id":15,"KnownIncidentID":0},{"_id":16,"KnownIncidentID":0}]
+            // data.push(...valuesToPush)
+            dataValues = data;
+            console.log(dataValues)
+
             //data is an array whose element is in form
             //   {_id: 2, KnownIncidentID: 0, ... }
             var $widget = $("#" + cLine.elemID).getWidgetParent();
             $widget.find(".filter-bar").height(25);
             var height = $widget.find(".grid-stack-item-content").innerHeight();
-            height -= $widget.find(".filter-bar").outerHeight(true) + 15;
+            height -= $widget.find(".filter-bar").outerHeight(true) + 20;
 
             return {
                data   : data,
                //columns[0] is Ox
                columns: [{id: "_id", label: "id"}, {id:"KnownIncidentID", label:"Incident"}],
-               ylabel : "Incident value",
+               // ylabel : "Type of incident occured",
                height : height, //fulfill the widget
                //this "chart" option will override the "chart" option few lines below
-               chart  : { axis : {x: { tick: {count: 20}} }}
+               // chart  : { axis : {x: { tick: {count: 20}} }}
             };
          },
       },
@@ -87,18 +100,68 @@ ReportFactory.createRcaReport = function(fPeriod){
       //note: the options may be slightly different as we use the version v0.4.11 - 2016-05-01
       chart: {
          data:{
-            type: "line"
+            type: "step",
+            labels: {
+               format: function (v, id, i, j) {
+                  var previousKnownIncidentID = dataValues[i - 1] ? dataValues[i - 1]["KnownIncidentID"] : undefined;
+                  if (v === 0 && (i === 0 || (i > 0 && v != previousKnownIncidentID))) {
+                     return null;
+                  } else if (v!==0 && i > 0 && v != previousKnownIncidentID) {
+                     return v;
+                  } else {
+                     return null; 
+                  }
+               }
+               
+            }
          },
+         subchart: {
+            show: true
+        },
          axis: {
             x: {
                type: "indexed", //see https://c3js.org/reference.html#axis-x-type
                tick: {
-                  format: function(val){ return val},
-                  count: 10,
+                  fit: true,
+                  centered: true,
+                  format: function(val){
+                     // var currentData = dataValues[parseIval - 1];
+                     let intVal = parseInt(val)
+                     if(!!dataValues[intVal-1] && !!dataValues[intVal]){
+                        var currentData = dataValues[intVal - 1];
+                        if (!!currentData && currentData.KnownIncidentID !== dataValues[intVal].KnownIncidentID) {
+                           console.log('cambio current id' + currentData._id);
+                           return parseInt(currentData._id);
+                        }
+                     }
+                     // console.log(dataValues); return parseInt(val)
+                  },
+                  count: 20,
+                  // culling:{
+                  //    max: 10
+                  // }
                },
-               min: 1,
-               max: 10
+               min: 0,
+               max: 80,
+               label: {
+                  text: 'Occurance time',
+                  position: 'outer-center'
+               }
             },
+            y:{
+               tick:{
+                  // format: function(val) {return parseInt(val)},
+                  format: d3.format('d'),
+                  values: [0,1,2,3]
+               },
+               padding: {
+                  top: 50
+               },
+               label: {
+                  text: 'Type of incident occured',
+                  position: 'outer-middle'
+               }
+            }
          },
          grid: {
             x: {
@@ -108,16 +171,16 @@ ReportFactory.createRcaReport = function(fPeriod){
          tooltip:{
             format: {
                title:  function(val){
-                  return "Hi " + val
+                  return "Detection point n " + val
                },
                value: function( value ){
-                  return value + " incidents";
+                  return "Incident of type " + value;
                }
             }
          },
          zoom: {
-            enabled: false,
-            rescale: false
+            enabled: true,
+            rescale: true
          },
       },
       afterEachRender: function (_chart) {
